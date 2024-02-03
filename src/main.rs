1f6fb22e7dae1e0436584b7d9a47a47e53ccae73
jbruws <vanmain.22@gmail.com>
2024-02-03 12:39:09 +0300

    Added formatting and image embeds

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,32 @@
 use actix_web::*;
+use indexmap::IndexMap;
 use serde::Deserialize;
 
 struct AppState<'a> {
     handle_formatter: handlebars::Handlebars<'a>,
-    template_post: String,
-    template_index: String,
 }
 
 #[derive(Deserialize)]
 pub struct PathInfo {
     postname_raw: String,
+}
+
+fn load_file(filepath: &str) -> String {
+    std::fs::read_to_string(format!("./{}", filepath))
+        .unwrap_or_else(|_| panic!("Can't read {}", &filepath))
+}
+
+fn md_to_html(raw_text: String) -> String {
+    let rules: IndexMap<String, String> =
+        serde_yaml::from_str(&load_file("./formatting.yaml")).unwrap();
+    let mut result = raw_text.clone();
+    for (template, expr) in rules.iter() {
+        result = regex::Regex::new(expr)
+            .unwrap()
+            .replace_all(&result, template)
+            .to_string();
+    }
+    result
 }
 
 #[get("/")]
@@ -26,11 +43,12 @@ async fn index(data: web::Data<AppState<'_>>) -> impl Responder {
             ));
         }
     }
+    let template_index = load_file("./templates/index_template.html");
     post_list.push_str("</ul>");
     HttpResponse::Ok().body(
         data.handle_formatter
             .render_template(
-                &data.template_index,
+                &template_index,
                 &serde_json::json!({"post_list": post_list}),
             )
             .unwrap(),
@@ -52,12 +70,12 @@ async fn blogpost(data: web::Data<AppState<'_>>, info: web::Path<PathInfo>) -> i
     if !md_posts.contains(&format!("./markdown/{}", postname)) {
         return HttpResponse::Ok().body("Not Found".to_string());
     }
-    let md_contents = std::fs::read_to_string(format!("./markdown/{}", postname))
-        .unwrap_or_else(|_| panic!("Can't read {}", &postname));
+    let md_contents = load_file(format!("./markdown/{}", postname).as_str());
+    let template_post = load_file("./templates/post_template.html");
     HttpResponse::Ok().body(
             data.handle_formatter.render_template(
-                &data.template_post,
-                &serde_json::json!({"post_title": info.postname_raw, "contents": md_contents, "footer": "<a href=\"..\">Back to index</a>".to_string()})
+                &template_post,
+                &serde_json::json!({"post_title": info.postname_raw, "contents": md_to_html(md_contents.to_string()), "footer": "<a href=\"..\">Back to index</a>".to_string()})
             ).unwrap()
         )
 }
@@ -68,8 +86,6 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .app_data(web::Data::new(AppState {
                 handle_formatter: handlebars::Handlebars::new(),
-                template_post: include_str!("../post_template.html").to_string(),
-                template_index: include_str!("../index_template.html").to_string(),
             }))
             .service(actix_files::Files::new("/web_data", "./web_data"))
             .service(blogpost)
